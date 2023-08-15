2d92fd6a5db2c7fee35286ca544d86c0058f66de
Sardorbek8877 <sardorbeksafarov8844@gmail.com>
2023-08-15 17:49:57 +0200

    Attendance History List

--- src/main/java/com/bek/hr_management_system/repository/AttendanceHistoryRepository.java
+++ src/main/java/com/bek/hr_management_system/repository/AttendanceHistoryRepository.java
@@ -2,9 +2,20 @@ package com.bek.hr_management_system.repository;
 
 import com.bek.hr_management_system.entity.AttendanceHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface AttendanceHistoryRepository extends JpaRepository<AttendanceHistory, UUID> {
 
+    List<AttendanceHistory> findAllByAttendanceUserId(UUID attendance_user_id);
+
+    @Query(value = "select * from attendance_history join " +
+            "attendance a on a.id = attendance_history.attendance_id " +
+            "where (a.user_id= :userId and attendance_history.date= :date)", nativeQuery = true)
+    List<AttendanceHistory> findByDateAndUserId(LocalDate date, UUID userId);
+
+    boolean existsByDate(LocalDate date);
 }
--- src/main/java/com/bek/hr_management_system/service/EmployeeService.java
+++ src/main/java/com/bek/hr_management_system/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.bek.hr_management_system.service;
 
+import com.bek.hr_management_system.entity.AttendanceHistory;
 import com.bek.hr_management_system.entity.Role;
 import com.bek.hr_management_system.entity.User;
 import com.bek.hr_management_system.payload.ApiResponse;
@@ -10,7 +11,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class EmployeeService {
@@ -28,6 +31,7 @@ public class EmployeeService {
     @Autowired
     UserRepository userRepository;
 
+    //GET ALL EMPLOYEES FOR  DIRECTOR OR MANAGER
     public ApiResponse getAllEmployee(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User user = (User) authentication.getPrincipal();
@@ -42,4 +46,24 @@ public class EmployeeService {
         return new ApiResponse("Cannot get the list", false);
     }
 
+    //GET ATTENDANCE LIST FOR ONE EMPLOYEE
+    public ApiResponse getAllAttendanceForOneEmployee(UUID id){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
+        Set<Role> roles = user.getRoles();
+        for (Role role : roles){
+            if (role.getName().name().equals("MANAGER") || role.getName().name().equals("WORKER")){
+                return new ApiResponse("Director and HR_Manager can get Attendance List", false);
+            }
+        }
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isEmpty()){
+            return new ApiResponse("User not found", false);
+        }
+        List<AttendanceHistory> attendanceHistoryList = attendanceHistoryRepository.findAllByAttendanceUserId(id);
+        if (attendanceHistoryList.isEmpty()){
+            return new ApiResponse("Attendance History not found", false);
+        }
+        return new ApiResponse("Attendance History List: ", true, attendanceHistoryList);
+    }
 }
